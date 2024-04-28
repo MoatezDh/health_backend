20738b38fee2b53a33b9db26c278f53e5d1207ee
MoatezDh <74154294+MoatezDh@users.noreply.github.com>
2024-04-28 02:07:52 +0200

    Update views.py

--- health/views.py
+++ health/views.py
@@ -2,7 +2,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import HealthSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import pandas as pd
